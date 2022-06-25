2c5e10545430cf27087335767662e36e10401446
Vlad Hrusha <hrushavladyslavwork@gmail.com>
2022-06-26 00:36:07 +0300

    Composing Meal Pages fixed, refactored

--- Task_2.2/scripts/index.js
+++ Task_2.2/scripts/index.js
@@ -1,67 +1,91 @@
-let navButtons = document.querySelectorAll('.nav__buttons');
-let section = document.querySelector('.menu');
 import food from '../data/food-items.json' assert { type: 'json' };
 
-for (let i = 0; i < navButtons.length; i++) {
-  navButtons[i].addEventListener('click', changeCategory, false);
-}
+addNavButtonsEvent();
 
 function changeCategory() {
-  let keys = Object.keys(food);
   let category = this.innerHTML;
-  let mealType = 0;
-
-  // console.log(food);
-  console.log(keys);
-
-  console.log(food.shakes.length);
-
-  section.innerHTML = '';
-  createMenuColumn(keys, category, mealType);
+  createMenuColumns(category);
 }
 
-function createMenuColumn(keys, category, mealType) {
-  let menuColumn = '';
+function createMenuColumns(category) {
+  let menu = document.querySelector('.menu');
+  menu.innerHTML = '';
 
-  menuColumn += '\t  <div class="menu__column">\n';
-  if (keys[0] == category.toLowerCase()) {
-    for (let i = 0; i < food.breakfast.length; i += 2) {
-      menuColumn += createMenuItem(food.breakfast, i);
+  let mealTypes = Object.keys(food);
+  let mealTypeString;
+  let mealTypeAssigned = true;
+  let mealType;
+
+  let menuColumn;
+
+  for (let i = 0; i < mealTypes.length; i++) {
+    if (mealTypes[i] == category.toLowerCase()) {
+      mealTypeString = mealTypes[i];
     }
   }
-  menuColumn += '\t\t  </div>\n';
 
-  section.innerHTML += menuColumn;
-
-  menuColumn = '';
-  menuColumn += '\t  <div class="menu__column">\n';
-  if (keys[0] == category.toLowerCase()) {
-    for (let i = 1; i < food.breakfast.length; i += 2) {
-      menuColumn += createMenuItem(food.breakfast, i);
-    }
+  switch (mealTypeString) {
+    case 'breakfast':
+      mealType = food.breakfast;
+      break;
+    case 'dinner':
+      mealType = food.dinner;
+      break;
+    case 'lunch':
+      mealType = food.lunch;
+      break;
+    case 'shakes':
+      mealType = food.shakes;
+      break;
+    default:
+      mealTypeAssigned = false;
   }
-  menuColumn += '\t\t  </div>\n';
 
-  section.innerHTML += menuColumn;
+  console.log(mealTypeAssigned);
+
+  if (mealTypeAssigned == true) {
+    menuColumn = '';
+    menuColumn += '\t  <div class="menu__column">\n';
+    for (let i = 0; i < mealType.length; i += 2) {
+      menuColumn += createMenuItem(mealType[i]);
+    }
+    menuColumn += '\t\t  </div>\n';
+
+    menu.innerHTML += menuColumn;
+
+    menuColumn = '';
+    menuColumn += '\t  <div class="menu__column">\n';
+    for (let i = 1; i < mealType.length; i += 2) {
+      menuColumn += createMenuItem(mealType[i]);
+    }
+    menuColumn += '\t\t  </div>\n';
+
+    menu.innerHTML += menuColumn;
+  }
 }
 
-function createMenuItem(category, i) {
-  console.log(category[0].imageURL);
+function createMenuItem(mealType) {
   let menuItem = '';
-
   menuItem += '\t\t<article class="menu__items">\n';
-  menuItem += `<img class="item__image" src="${category[i].imageURL}">\n`;
+  menuItem += `<img class="item__image" src="${mealType.imageURL}">\n`;
   menuItem += '\t\t  <div class="item__description">\n';
   menuItem += '\t\t\t<header class="item__header">\n';
-  menuItem += `\t\t\t  <h5>${category[i].name}</h5>\n`;
-  menuItem += `\t\t\t  <h5 class="item__price">${category[i].price}</h5>\n`;
+  menuItem += `\t\t\t  <h5>${mealType.name}</h5>\n`;
+  menuItem += `\t\t\t  <h5 class="item__price">${mealType.price}</h5>\n`;
   menuItem += '\t\t\t</header>\n';
   menuItem += '\t\t\t<p class="item__paragraph">\n';
-  menuItem += `\t\t\t  ${category[i].description}\n`;
+  menuItem += `\t\t\t  ${mealType.description}\n`;
   menuItem +=
     '\t\t\t  quis ligula at mauris tempus auctor. Ut fermentum lacus et.\n';
   menuItem += '\t\t\t</p>\n';
   menuItem += '\t\t  </div>\n';
   menuItem += '\t\t</article>\n';
   return menuItem;
+}
+
+function addNavButtonsEvent() {
+  let navButtons = document.querySelectorAll('.nav__buttons');
+  for (let i = 0; i < navButtons.length; i++) {
+    navButtons[i].addEventListener('click', changeCategory, false);
+  }
 }
