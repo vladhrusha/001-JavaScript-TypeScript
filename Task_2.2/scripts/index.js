6986ba22158c783c4102a60e56234b9884293339
Vlad Hrusha <hrushavladyslavwork@gmail.com>
2022-06-27 15:55:28 +0300

    javascript refactored

--- Task_2.2/scripts/index.js
+++ Task_2.2/scripts/index.js
@@ -1,31 +1,60 @@
 import food from '../data/food-items.json' assert { type: 'json' };
 
 addNavButtonsEvent();
-createAllFoodTypes();
-function changeCategory() {
-  let category = this.innerHTML;
-  createMenuColumns(category);
+createAllFoodTypesMenu();
+
+function addNavButtonsEvent() {
+  let navButtons = document.querySelectorAll('.nav__buttons');
+  navButtons[0].addEventListener('click', createAllFoodTypesMenu, false);
+  for (let i = 1; i < navButtons.length; i++) {
+    navButtons[i].addEventListener('click', changeCategory, false);
+  }
 }
 
-function createMenuColumns(category) {
+function changeCategory() {
+  let category = this.innerHTML;
+  createMenu(category);
+}
+
+function createMenu(category) {
   let menu = document.querySelector('.menu');
   menu.innerHTML = '';
+  menu.innerHTML += '\t  <div class="menu__column">\n';
+  menu.innerHTML += '\t  <div class="menu__column">\n';
 
   let mealTypes = Object.keys(food);
-  console.log(mealTypes);
   let mealTypeString;
-  let mealTypeAssigned = true;
   let mealType;
-
-  let menuColumn;
-  menu.innerHTML += '\t  <div class="menu__column">\n';
 
   for (let i = 0; i < mealTypes.length; i++) {
     if (mealTypes[i] == category.toLowerCase()) {
       mealTypeString = mealTypes[i];
     }
   }
+  mealType = returnMealTypeObject(mealTypeString);
 
+  createMenuColumns(menu, mealType);
+}
+
+function createAllFoodTypesMenu() {
+  let menu = document.querySelector('.menu');
+  menu.innerHTML = '';
+  menu.innerHTML += '\t  <div class="menu__column">\n';
+  menu.innerHTML += '\t  <div class="menu__column">\n';
+
+  let mealTypes = Object.keys(food);
+  let mealTypeString;
+  let mealType;
+
+  for (let i = 0; i < mealTypes.length - 1; i++) {
+    mealTypeString = mealTypes[i];
+    mealType = returnMealTypeObject(mealTypeString);
+    createMenuColumns(menu, mealType);
+  }
+}
+
+function returnMealTypeObject(mealTypeString) {
+  let mealType;
   switch (mealTypeString) {
     case 'breakfast':
       mealType = food.breakfast;
@@ -39,78 +68,21 @@ function createMenuColumns(category) {
     case 'shakes':
       mealType = food.shakes;
       break;
-    default:
-      createAllFoodTypes();
-      mealTypeAssigned = false;
   }
-
-  if (mealTypeAssigned == true) {
-    menuColumn = '';
-    menuColumn += '\t  <div class="menu__column">\n';
-    for (let i = 0; i < mealType.length; i += 2) {
-      menu.querySelectorAll('.menu__column')[0].innerHTML += createMenuItem(
-        mealType[i]
-      );
-    }
-    menuColumn += '\t\t  </div>\n';
-
-    menu.innerHTML += menuColumn;
-
-    menuColumn = '';
-    menuColumn += '\t  <div class="menu__column">\n';
-    for (let i = 1; i < mealType.length; i += 2) {
-      menu.querySelectorAll('.menu__column')[1].innerHTML += createMenuItem(
-        mealType[i]
-      );
-    }
-    menuColumn += '\t\t  </div>\n';
-
-    menu.innerHTML += menuColumn;
-  }
+  return mealType;
 }
 
-function createAllFoodTypes() {
-  let menu = document.querySelector('.menu');
-  menu.innerHTML = '';
+function createMenuColumns(menu, mealType) {
+  for (let i = 0; i < mealType.length; i += 2) {
+    menu.querySelectorAll('.menu__column')[0].innerHTML += createMenuItem(
+      mealType[i]
+    );
+  }
 
-  let mealTypes = Object.keys(food);
-  let mealTypeString;
-  let mealTypeAssigned = true;
-  let mealType;
-
-  menu.innerHTML += '\t  <div class="menu__column">\n';
-  menu.innerHTML += '\t  <div class="menu__column">\n';
-  for (let i = 0; i < mealTypes.length - 1; i++) {
-    mealTypeString = mealTypes[i];
-
-    switch (mealTypeString) {
-      case 'breakfast':
-        mealType = food.breakfast;
-        break;
-      case 'dinner':
-        mealType = food.dinner;
-        break;
-      case 'lunch':
-        mealType = food.lunch;
-        break;
-      case 'shakes':
-        mealType = food.shakes;
-        break;
-      default:
-        mealTypeAssigned = false;
-    }
-
-    for (let i = 0; i < mealType.length; i += 2) {
-      menu.querySelectorAll('.menu__column')[0].innerHTML += createMenuItem(
-        mealType[i]
-      );
-    }
-
-    for (let i = 1; i < mealType.length; i += 2) {
-      menu.querySelectorAll('.menu__column')[1].innerHTML += createMenuItem(
-        mealType[i]
-      );
-    }
+  for (let i = 1; i < mealType.length; i += 2) {
+    menu.querySelectorAll('.menu__column')[1].innerHTML += createMenuItem(
+      mealType[i]
+    );
   }
 }
 
@@ -131,11 +103,4 @@ function createMenuItem(meal) {
   menuItem += '\t\t  </div>\n';
   menuItem += '\t\t</article>\n';
   return menuItem;
-}
-
-function addNavButtonsEvent() {
-  let navButtons = document.querySelectorAll('.nav__buttons');
-  for (let i = 0; i < navButtons.length; i++) {
-    navButtons[i].addEventListener('click', changeCategory, false);
-  }
 }
