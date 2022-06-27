37723801833226b8478de946b88eac7d8f822f49
Vlad Hrusha <hrushavladyslavwork@gmail.com>
2022-06-27 15:23:13 +0300

    All food menu added (Backup)

--- Task_2.2/scripts/index.js
+++ Task_2.2/scripts/index.js
@@ -1,7 +1,7 @@
 import food from '../data/food-items.json' assert { type: 'json' };
 
 addNavButtonsEvent();
-
+createAllFoodTypes();
 function changeCategory() {
   let category = this.innerHTML;
   createMenuColumns(category);
@@ -12,11 +12,13 @@ function createMenuColumns(category) {
   menu.innerHTML = '';
 
   let mealTypes = Object.keys(food);
+  console.log(mealTypes);
   let mealTypeString;
   let mealTypeAssigned = true;
   let mealType;
 
   let menuColumn;
+  menu.innerHTML += '\t  <div class="menu__column">\n';
 
   for (let i = 0; i < mealTypes.length; i++) {
     if (mealTypes[i] == category.toLowerCase()) {
@@ -38,16 +40,17 @@ function createMenuColumns(category) {
       mealType = food.shakes;
       break;
     default:
+      createAllFoodTypes();
       mealTypeAssigned = false;
   }
-
-  console.log(mealTypeAssigned);
 
   if (mealTypeAssigned == true) {
     menuColumn = '';
     menuColumn += '\t  <div class="menu__column">\n';
     for (let i = 0; i < mealType.length; i += 2) {
-      menuColumn += createMenuItem(mealType[i]);
+      menu.querySelectorAll('.menu__column')[0].innerHTML += createMenuItem(
+        mealType[i]
+      );
     }
     menuColumn += '\t\t  </div>\n';
 
@@ -56,7 +59,9 @@ function createMenuColumns(category) {
     menuColumn = '';
     menuColumn += '\t  <div class="menu__column">\n';
     for (let i = 1; i < mealType.length; i += 2) {
-      menuColumn += createMenuItem(mealType[i]);
+      menu.querySelectorAll('.menu__column')[1].innerHTML += createMenuItem(
+        mealType[i]
+      );
     }
     menuColumn += '\t\t  </div>\n';
 
@@ -64,17 +69,62 @@ function createMenuColumns(category) {
   }
 }
 
-function createMenuItem(mealType) {
+function createAllFoodTypes() {
+  let menu = document.querySelector('.menu');
+  menu.innerHTML = '';
+
+  let mealTypes = Object.keys(food);
+  let mealTypeString;
+  let mealTypeAssigned = true;
+  let mealType;
+
+  menu.innerHTML += '\t  <div class="menu__column">\n';
+  menu.innerHTML += '\t  <div class="menu__column">\n';
+  for (let i = 0; i < mealTypes.length - 1; i++) {
+    mealTypeString = mealTypes[i];
+
+    switch (mealTypeString) {
+      case 'breakfast':
+        mealType = food.breakfast;
+        break;
+      case 'dinner':
+        mealType = food.dinner;
+        break;
+      case 'lunch':
+        mealType = food.lunch;
+        break;
+      case 'shakes':
+        mealType = food.shakes;
+        break;
+      default:
+        mealTypeAssigned = false;
+    }
+
+    for (let i = 0; i < mealType.length; i += 2) {
+      menu.querySelectorAll('.menu__column')[0].innerHTML += createMenuItem(
+        mealType[i]
+      );
+    }
+
+    for (let i = 1; i < mealType.length; i += 2) {
+      menu.querySelectorAll('.menu__column')[1].innerHTML += createMenuItem(
+        mealType[i]
+      );
+    }
+  }
+}
+
+function createMenuItem(meal) {
   let menuItem = '';
   menuItem += '\t\t<article class="menu__items">\n';
-  menuItem += `<img class="item__image" src="${mealType.imageURL}">\n`;
+  menuItem += `<img class="item__image" src="${meal.imageURL}">\n`;
   menuItem += '\t\t  <div class="item__description">\n';
   menuItem += '\t\t\t<header class="item__header">\n';
-  menuItem += `\t\t\t  <h5>${mealType.name}</h5>\n`;
-  menuItem += `\t\t\t  <h5 class="item__price">${mealType.price}</h5>\n`;
+  menuItem += `\t\t\t  <h5>${meal.name}</h5>\n`;
+  menuItem += `\t\t\t  <h5 class="item__price">${meal.price}</h5>\n`;
   menuItem += '\t\t\t</header>\n';
   menuItem += '\t\t\t<p class="item__paragraph">\n';
-  menuItem += `\t\t\t  ${mealType.description}\n`;
+  menuItem += `\t\t\t  ${meal.description}\n`;
   menuItem +=
     '\t\t\t  quis ligula at mauris tempus auctor. Ut fermentum lacus et.\n';
   menuItem += '\t\t\t</p>\n';
