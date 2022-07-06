8ba172e3e5ed7a2efb1ecb5d0f5cabd93e11c8ce
Vlad Hrusha <hrushavladyslavwork@gmail.com>
2022-07-06 19:02:59 +0300

    Update index.js

--- Task_2.5/scripts/index.js
+++ Task_2.5/scripts/index.js
@@ -2,9 +2,8 @@ import {
   addCurrentWeather,
   addForecast,
 } from './OpenWeather/fetchingOpenWeatherData.js';
+
 const form = document.querySelector('.form');
-const table = document.querySelector('table');
-let regionNames = new Intl.DisplayNames(['en'], { type: 'region' });
 form.addEventListener('submit', citySearch);
 
 citySearch();
@@ -14,10 +13,15 @@ function citySearch(event) {
     event.preventDefault();
   }
 
+  let regionNames = new Intl.DisplayNames(['en'], { type: 'region' });
+
+  const table = document.querySelector('table');
   let input = form.search.value.split(', ');
   let city = input[0];
   let areaCode = input[1];
+
   const APIkey = '73ff54cf7854273a427a0750b527fac9';
+
   const currentWeatherAPIcall = `https://api.openweathermap.org/data/2.5/weather?units=metric&q=${city}&appid=${APIkey}`;
   const geocodingAPIcall = `http://api.openweathermap.org/geo/1.0/direct?q=${city},${areaCode}&limit=1&appid=${APIkey}`;
 
