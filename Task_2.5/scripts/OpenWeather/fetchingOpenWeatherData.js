36dadd250bca45cae5c5e083e8f6d7eb25752abf
Vlad Hrusha <hrushavladyslavwork@gmail.com>
2022-07-06 18:32:27 +0300

    adding OpenWeather data modularized

---
+++ Task_2.5/scripts/OpenWeather/fetchingOpenWeatherData.js
@@ -0,0 +1,51 @@
+import {
+  addTableBodyRow,
+  addTableHead,
+} from '../Table/tableCreationFunctions.js';
+
+let regionNames = new Intl.DisplayNames(['en'], { type: 'region' });
+const table = document.querySelector('table');
+
+function addCurrentWeather(currentWeatherAPIcall) {
+  fetch(currentWeatherAPIcall)
+    .then((response) => response.json())
+    .then((currentWeatherData) => {
+      let tableRowData = {};
+      tableRowData.weatherMain = currentWeatherData.weather[0].main;
+      tableRowData.icon = currentWeatherData.weather[0].icon;
+      tableRowData.temperature = Math.round(currentWeatherData.main.temp);
+      tableRowData.temperatureFellsLike = Math.round(
+        currentWeatherData.main.feels_like
+      );
+      tableRowData.city = currentWeatherData.name;
+      tableRowData.country = regionNames.of(currentWeatherData.sys.country);
+      table.innerHTML += addTableHead(tableRowData);
+    });
+}
+
+function addForecast(oneAPIcall) {
+  fetch(oneAPIcall)
+    .then((response) => response.json())
+    .then((forecastData) => {
+      let tableRowData = {};
+      let tableBody = `<tbody>`;
+      forecastData.daily.forEach((day, i) => {
+        if (i < 5) {
+          tableRowData.weatherDescription = day.weather[0].description;
+          tableRowData.weatherMain = day.weather[0].main;
+          tableRowData.icon = day.weather[0].icon;
+          tableRowData.temperatureMax = Math.round(day.temp.max);
+          tableRowData.temperatureMin = Math.round(day.temp.min);
+          tableRowData.day = new Date(day.dt * 1000).toLocaleString('default', {
+            weekday: 'short',
+          });
+          tableBody += addTableBodyRow(tableRowData);
+        }
+      });
+
+      tableBody += `</tbody>`;
+      table.innerHTML += tableBody;
+    });
+}
+
+export { addCurrentWeather, addForecast };
--- Task_2.5/scripts/index.js
+++ Task_2.5/scripts/index.js
@@ -1,8 +1,7 @@
 import {
-  addTableBodyRow,
-  addTableHead,
-} from './Table/tableCreationFunctions.js';
-
+  addCurrentWeather,
+  addForecast,
+} from './OpenWeather/fetchingOpenWeatherData.js';
 const form = document.querySelector('.form');
 const table = document.querySelector('table');
 let regionNames = new Intl.DisplayNames(['en'], { type: 'region' });
@@ -28,7 +27,7 @@ function citySearch(event) {
 
   table.innerHTML = '';
   //add current weather
-  fetchCurrentData(currentWeatherAPIcall);
+  addCurrentWeather(currentWeatherAPIcall);
   //add forecast
   fetch(geocodingAPIcall)
     .then((response) => response.json())
@@ -36,51 +35,9 @@ function citySearch(event) {
       let lat = geolocationData[0].lat;
       let lon = geolocationData[0].lon;
       const oneAPIcall = `https://api.openweathermap.org/data/3.0/onecall?units=metric&lat=${lat}&lon=${lon}&exclude=hourly, current, minutely, alerts&appid=${APIkey}`;
-      fetchForecastData(oneAPIcall);
+      addForecast(oneAPIcall);
     })
     .catch((error) => {
       alert('Invalid City');
-    });
-}
-
-function fetchCurrentData(currentWeatherAPIcall) {
-  fetch(currentWeatherAPIcall)
-    .then((response) => response.json())
-    .then((currentWeatherData) => {
-      let tableRowData = {};
-      tableRowData.weatherMain = currentWeatherData.weather[0].main;
-      tableRowData.icon = currentWeatherData.weather[0].icon;
-      tableRowData.temperature = Math.round(currentWeatherData.main.temp);
-      tableRowData.temperatureFellsLike = Math.round(
-        currentWeatherData.main.feels_like
-      );
-      tableRowData.city = currentWeatherData.name;
-      tableRowData.country = regionNames.of(currentWeatherData.sys.country);
-      table.innerHTML += addTableHead(tableRowData);
-    });
-}
-
-function fetchForecastData(oneAPIcall) {
-  fetch(oneAPIcall)
-    .then((response) => response.json())
-    .then((forecastData) => {
-      let tableRowData = {};
-      let tableBody = `<tbody>`;
-      forecastData.daily.forEach((day, i) => {
-        if (i < 5) {
-          tableRowData.weatherDescription = day.weather[0].description;
-          tableRowData.weatherMain = day.weather[0].main;
-          tableRowData.icon = day.weather[0].icon;
-          tableRowData.temperatureMax = Math.round(day.temp.max);
-          tableRowData.temperatureMin = Math.round(day.temp.min);
-          tableRowData.day = new Date(day.dt * 1000).toLocaleString('default', {
-            weekday: 'short',
-          });
-          tableBody += addTableBodyRow(tableRowData);
-        }
-      });
-
-      tableBody += `</tbody>`;
-      table.innerHTML += tableBody;
     });
 }
